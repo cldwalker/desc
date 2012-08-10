8f85467c76dce58f5959ef0e5bdecd11094845d6
Gabriel Horner <gabriel.horner@gmail.com>
2012-08-10 16:24:17 -0400

    init with project.clj filled out

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject desc "0.1.0-SNAPSHOT"
+  :description "Describe what you want about clojure."
+  :url "http://github.com/cldwalker/desc"
+  :license {:name "The MIT LICENSE"
+            :url "https://en.wikipedia.org/wiki/MIT_License"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
---
+++ test/desc/core_test.clj
@@ -0,0 +1,7 @@
+(ns desc.core-test
+  (:use clojure.test
+        desc.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
