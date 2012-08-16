256e8e0484cf3e811f4619adb72737340af56652
Gabriel Horner <gabriel.horner@gmail.com>
2012-08-15 21:30:04 -0400

    clean records between facts

--- test/desc/core_test.clj
+++ test/desc/core_test.clj
@@ -3,9 +3,12 @@
         desc.core)
   (:require [clojure.java.io :as io]))
 
+; before-all: create temp test-dir
 (def test-dir (-> (io/resource *file*) .getPath io/file .getParentFile (io/file "tmp")))
 (.mkdir test-dir)
 (System/setProperty "user.home" (str test-dir))
+
+(background (before :facts (reset! records nil)))
 
 (fact "nonexistent .desc.clj yields vector"
   (@#'desc.core/check-records)
@@ -20,6 +23,6 @@
   (desc "comp" "just handy")
   (@#'desc.core/fetch-records) => [{:name "comp" :desc "just handy"}])
 
-; cleanup test-dir
+; after-all: cleanup test-dir
 (.delete (io/file test-dir ".desc.clj"))
 (.delete test-dir)
